7f9348db3e17ae93a63fe14c37f7c35577ca219a
Obed Garcia <obed1355@gmail.com>
2020-04-11 22:51:48 -0400

    add csv handler

--- use_cases/case_api/sitka_highs.py
+++ use_cases/case_api/sitka_highs.py
@@ -1,9 +1,35 @@
 import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
 
 filename = 'data/sitka_weather_07-2018_simple.csv'
 with open(filename) as fo:
     reader = csv.reader(fo)
     header_row = next(reader)
 
-    for index, column_header in enumerate(header_row):
-        print(index, column_header)
+    # for index, column_header in enumerate(header_row):
+    #     print(index, column_header)
+
+    dates, highs, lows = [], [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        high = int(row[5])
+        low = int(row[6])
+        highs.append(high)
+        dates.append(current_date)
+        lows.append(low)
+
+plt.style.use('bmh')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c='blue', alpha=0.5)
+
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+plt.title('Dates and lows Temperatures', fontsize=24)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel('Temperature', fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
+
